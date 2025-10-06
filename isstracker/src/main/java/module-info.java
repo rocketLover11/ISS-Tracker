3c0fd470012baf4150df9bde434d9243b76651e5
rocketLover11 <gidafish@gmail.com>
2025-10-06 22:23:20 +0000

    1.0 testing

---
+++ isstracker/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.rocketlover11 {
+    // Ensure JavaFX modules are available on the module path when running
+    requires javafx.controls;
+    requires javafx.web;
+    requires org.json;
+
+    exports com.rocketlover11;
+}
